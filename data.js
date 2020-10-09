a8b2bd8e7d50bd68cdf1c19e87e917fb557fb400
Stefan Vlajich <vlaj0004@algonquinlive.com>
2020-10-09 13:59:12 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,22 @@
+/**
+ * Hybrid 7 JS file
+ * Name: Stefan Vlajich
+ */
+
+const data = {
+  name: 'Stefan Vlajich',
+  characters: [
+    { id: 1, name: 'Frodo', race: 'Hobbit' },
+    { id: 1, name: 'Gandalf', race: 'Wizard' },
+    { id: 1, name: 'Gimli', race: 'Dwarf' },
+    { id: 1, name: 'Legolas', race: 'Elf' },
+    { id: 1, name: 'Boromir', race: 'Human' },
+    { id: 1, name: 'TreeBeard', race: 'Ent' },
+    { id: 1, name: 'Gollum', race: 'Stoor Hobbit' },
+    { id: 1, name: 'Azog', race: 'Orc' },
+    { id: 1, name: 'Beorn', race: 'Shapeshifter' },
+    { id: 1, name: 'Shelob', race: 'Spider' },
+  ],
+};
+
+module.exports = data;
